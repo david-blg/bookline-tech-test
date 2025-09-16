2c42decd6c14b0c548283bfe115ec13bf2090b88
david-blg <dav.stack.dev@gmail.com>
2025-09-16 11:19:14 +0200

    feat: add centralized logging config

--- app/api/routes/booking_routes.py
+++ app/api/routes/booking_routes.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
-
+from app.core.logger.setup_logger import logger
 
 router = APIRouter(prefix="/bookings", tags=["bookings"])
 
 
 @router.get("/")
 def get_bookings():
+    logger.info("Getting all bookings")
     return {"message": "get bookings"}
--- app/core/logger/setup_logger.py
+++ app/core/logger/setup_logger.py
@@ -1,1 +1,30 @@
-# logger setup....
+import logging
+import sys
+from pathlib import Path
+
+def setup_logger(name: str = 'car_rental_api'):
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if logger.handlers:
+        return logger
+    
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+
+    log_file = Path("logs/app.log")
+    log_file.parent.mkdir(parents=True, exist_ok=True)
+    
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    logger.info("Logger configured successfully")
+    return logger
+
+
+logger = setup_logger()
